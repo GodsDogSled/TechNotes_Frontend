c74ea81d86acd9021dc80a2072636fdd48f1db19
GodsDogSled <gabekelly@hotmail.ca>
2024-09-12 14:23:32 -0700

    logged in dash board ui set up and basic redux configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Public from './components/Public'
-import Login from "./components/Login"
-import './App.css'
+import Login from "./features/auth/Login"
+import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
+import UsersList from './features/users/UsersList'
+import NotesList from './features/notes/NotesList'
+// import './App.css'
 
 function App() {
-
 
   return (
     <>
@@ -14,6 +17,19 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Public />} />
           <Route path="login" element={<Login />} />
+
+          <Route path='dash' element={<DashLayout />} >
+
+            <Route index element={<Welcome />} />
+            <Route path="notes" >
+              <Route index element={<NotesList />} />
+            </Route>
+            <Route path="users" >
+              <Route index element={<UsersList />} />
+            </Route>
+
+          </Route>{/*End Dash} */}
+
         </Route>
       </Routes>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
+import { store } from './app/store.tsx'
+import { Provider } from 'react-redux'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 )
